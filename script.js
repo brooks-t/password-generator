2ac2245c82a6762cc40a2b1ab53d0f99e30fa621
Brooks Tiffany <brooks.t.tiffany@gmail.com>
2021-03-24 11:16:58 -0700

    created empty variables and arrays to be filled

--- script.js
+++ script.js
@@ -4,8 +4,11 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword(){
   //TODO: your code here
-  
-
+  var passLength = 0;
+  var passChars = [];
+  var passNums = [];
+  var passUppercase = [];
+  var passLowercase = [];
 }
 
 // Write password to the #password input
