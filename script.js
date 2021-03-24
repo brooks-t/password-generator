1f282cbe8077f49a17330a80c7635d589ef21baf
Brooks Tiffany <brooks.t.tiffany@gmail.com>
2021-03-24 16:25:11 -0700

    adding comments and cleaning up script.js

--- script.js
+++ script.js
@@ -2,11 +2,8 @@
 //DO NOT CHANGE THIS CODE
 var generateBtn = document.querySelector("#generate");
 
-
-  //TODO: your code here
-
-
 function generatePassword(){
+  //declaring initial variables
   var passLength = 8;
   var passSpec = true;
   var passNum = true;
@@ -14,18 +11,21 @@ function generatePassword(){
   var passLower = true;
   var myPassword = "";
 
+  //building my arrays
   var specArray = ["!", "#", "$", "%", "&", "(", ")", "*", "+", "<", ">", "=", "?", "@", "^", "{", "}", "~"];
   var numArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
   var upperArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
   var lowerArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
+  //prompt asking for length of password
   function funcLength() {
     passLength = prompt("How long would you like your password to be? Please choose a number between 8 and 128.");
+    //checking to make sure it's a number and that it's not too short or too long
     if (isNaN(passLength)) {
-      alert("Please enter a number between 8 and 128");
+      alert("Oops! That's not a number. Please enter a number between 8 and 128");
       funcLength();
       } else if (passLength < 8 || passLength > 128) {
-        alert("Please enter a number between 8 and 128");
+        alert("Oops! That's too short or too long. Please enter a number between 8 and 128");
         funcLength();
       }
       else {
@@ -33,6 +33,7 @@ function generatePassword(){
       } 
   }
 
+  //the following four functions confirm whether or not the user wants to include that character type
   function funcSpecial() {
     passSpec = confirm("Would you like to include special characters in your password?");
     return passSpec;
@@ -53,9 +54,10 @@ function generatePassword(){
     return passLower;
   }
 
+  //this function makes sure that the user has chosen at least one character type to include
   function passCheck() {
     if (passSpec === false && passNum === false && passUpper === false && passLower === false) {
-      alert("Error. You must include atleast one character type in your password. Please try again.");
+      alert("Utoh! You must include atleast one character type in your password. Please try again.");
       generatePassword();
     }
     else {
@@ -63,8 +65,7 @@ function generatePassword(){
     }
   }
 
-  //TODO: create function that takes all of user inputs and generates a password then returns that password
-
+  //this function is where the magic happens. the password is randomly generated from the four arrays up to the user's desired length
   function randomizePass() {
     var randomCharacter = "";
     for (var i = 0; i < passLength; i++) {
@@ -86,6 +87,7 @@ function generatePassword(){
     return myPassword;
   }
 
+  //calling functions with logs for debugging
   funcLength();
   console.log("Chosen length: " + passLength);
 
@@ -105,9 +107,8 @@ function generatePassword(){
   console.log("Whew! Made it this far without any issues!");
 
   randomizePass();
-  console.log(myPassword);
+  console.log("G14 Classified Password: " + myPassword);
   return myPassword;
-
 }
 
 // Write password to the #password input
@@ -123,12 +124,3 @@ function writePassword() {
 // Add event listener to generate button
 // DO NOT CHANGE THIS CODE
 generateBtn.addEventListener("click", writePassword);
-
-
-/* for testing the arrays...  
-  console.log(passLength);
-  console.log(passSpecial);
-  console.log(passNumeric);
-  console.log(passUppercase);
-  console.log(passLowercase);
-  */
