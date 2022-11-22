24b5884a618c3e5e0cb6603f3600927df7ac24b7
Brooks Tiffany <brooks.t.tiffany@gmail.com>
2022-11-22 14:18:11 -0800

    some minor deletions

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// Assignment Code
-//DO NOT CHANGE THIS CODE
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword(){
@@ -112,7 +110,6 @@ function generatePassword(){
 }
 
 // Write password to the #password input
-/* DO NOT CHANGE THIS CODE!  This function is going to call your generatePassword() function and whatever is returned from that function will be put onto the page */
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -122,5 +119,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-// DO NOT CHANGE THIS CODE
 generateBtn.addEventListener("click", writePassword);
